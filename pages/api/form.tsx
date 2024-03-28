fb7a9922abd86b8a9ee290aa4b8cbd3eb6f1ff97
DenialOfKato <marcos@agencianaut.com.br>
2024-03-28 18:46:47 -0300

    Update form.tsx

--- pages/api/form.tsx
+++ pages/api/form.tsx
@@ -1,6 +1,6 @@
 import userController from "@/utils/controllers/UserController"
 
-export default async function handler(req, res) {
+export default async function handler(req: { body: any }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: void): void; new(): any } } }) {
     const response = await userController.saveUser(req.body)
     .then((data)=>{
         console.log(data)
